63e11374bae8c285a0036ac4c32893a8a43e7090
technillogue <technillogue@gmail.com>
2022-09-21 19:24:12 -0400

    forest version

--- pghelp.py
+++ pghelp.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3.9
-# Copyright (c) 2022 sylv
+# Copyright (c) 2021 MobileCoin Inc.
 # Copyright (c) 2021 The Forest Team
+
 import asyncio
 import copy
 import logging
 import os
 import time
-import json
 from contextlib import asynccontextmanager
 from typing import Any, AsyncGenerator, Callable, Optional, Union
-import asyncpg
 
-from . import utils
+try:
+    import asyncpg
+
+    DUMMY = False
+except ImportError:
+    from dummy_asyncpg import asyncpg
+
+    DUMMY = True
 
 
 Loop = Optional[asyncio.events.AbstractEventLoop]
 
+AUTOCREATE = "true" in os.getenv("AUTOCREATE_TABLES", "false").lower()
 MAX_RESP_LOG_LEN = int(os.getenv("MAX_RESP_LOG_LEN", "256"))
 LOG_LEVEL_DEBUG = bool(os.getenv("DEBUG", None))
 
@@ -39,9 +46,7 @@ class OneTruePool:
     connecting: Optional[asyncio.Future] = None
     pool: Optional[asyncpg.Pool] = None
 
-    async def connect(
-        self, url: str, table: str, database_name: Optional[str] = None
-    ) -> None:
+    async def connect(self, url: str, table: str) -> None:
         if not self.pool:
             if self.connecting:
                 await self.connecting
@@ -52,7 +57,7 @@ class OneTruePool:
                 # but counterproductive if you're proxying a database connection through localhost
                 # if "localhost" in self.database:
                 #     pool = await asyncpg.create_pool(user="postgres")
-                self.pool = await asyncpg.create_pool(url, database=database_name)
+                self.pool = await asyncpg.create_pool(url)
                 logging.debug("created pool %s for %s", self.pool, table)
                 self.connecting.set_result(True)
 
@@ -63,57 +68,25 @@ class OneTruePool:
             raise Exception("no pool, use pool.connect first")
         return self.pool.acquire()
 
-    closing_event = asyncio.Event()
-    listener_tasks: list[asyncio.Task] = []
-
     async def close(self) -> None:
-        logging.info("closing pool")
-        self.closing_event.set()
-        for listener_task in self.listener_tasks:
-            # wait for them to remove listners and return connections
-            # maybe add a timeout here just in case?
-            await listener_task
         try:
             if self.pool:
                 await self.pool.close()
         except (asyncpg.PostgresError, asyncpg.InternalClientError) as e:
             logging.error(e)
 
-    async def _start_listener_task(
-        self, channel: str, notification_handler: Callable
-    ) -> None:
-        async with self.acquire() as conn:
-            try:
-                await conn.add_listener(channel, notification_handler)
-                await self.closing_event.wait()
-                logging.info(
-                    "got closing event from %s, %s", channel, notification_handler
-                )
-            finally:
-                try:
-                    await conn.remove_listener(channel, notification_handler)
-                # conn was already released/closed
-                except asyncpg.InterfaceError:
-                    pass
-
-    async def start_listener(self, channel: str, handler: Callable) -> None:
-        task = asyncio.create_task(self._start_listener_task(channel, handler))
-        task.add_done_callback(utils.log_task_result)
-        self.listener_tasks.append(task)
-
 
 pool = OneTruePool()
 
 
 class SimpleInterface:
-    def __init__(self, database: str, database_name: Optional[str] = None) -> None:
+    def __init__(self, database: str) -> None:
         self.database = database
-        self.database_name = database_name
 
     @asynccontextmanager
     async def get_connection(self) -> AsyncGenerator:
         if not pool.pool:
-            await pool.connect(self.database, "simple interface", self.database_name)
+            await pool.connect(self.database, "simple interface")
         assert pool.pool
         async with pool.acquire() as conn:
             logging.info("connection acquired")
@@ -130,10 +103,10 @@ class PGExpressions(dict):
                 "exists"
             ] = f"SELECT * FROM pg_tables WHERE tablename = '{self.table}';"
         if "create_table" not in self:
-            logging.warning(f"'create_table' not defined for {self.table}")
+            self.logger.warning(f"'create_table' not defined for {self.table}")
 
     def get_query(self, key: str) -> str:
-        logging.debug(f"self.get invoked for {key}")
+        self.logger.debug(f"self.get invoked for {key}")
         return dict.__getitem__(self, key).replace("{self.table}", self.table)
 
 
@@ -144,12 +117,7 @@ class PGInterface:
     - and an optional event loop"""
 
     def __init__(
-        self,
-        query_strings: PGExpressions,
-        database: str = "",
-        loop: Loop = None,
-        database_name: Optional[str] = None,
-        pool: OneTruePool = pool,
+        self, query_strings: PGExpressions, database: str = "", loop: Loop = None
     ) -> None:
         """Accepts a PGExpressions argument containing postgresql expressions, a database string, and an optional event loop."""
 
@@ -157,7 +125,6 @@ class PGInterface:
         self.database: Union[str, dict] = copy.deepcopy(
             database
         )  # either a db uri or canned resps
-        self.database_name = database_name
         self.queries = query_strings
         self.table = self.queries.table
         self.MAX_RESP_LOG_LEN = MAX_RESP_LOG_LEN
@@ -167,76 +134,69 @@ class PGInterface:
         self.logger = get_logger(
             f'{self.table}{"_fake" if not self.database else ""}_interface'
         )
-        self.pool = pool
+
+    def finish_init(self) -> None:
+        """Optionally triggers creating tables and checks existence."""
+        if not pool.pool:
+            self.logger.warning("RUNNING IN FAKE MODE")
+        if pool.pool and self.table and not self.sync_exists():
+            if AUTOCREATE:
+                self.sync_create_table()
+                self.logger.warning(f"building table {self.table}")
+            else:
+                self.logger.warning(
+                    f"not autocreating! table: {self.table} does not exist!"
+                )
+        for k in self.queries:
+            if AUTOCREATE and "create" in k and "index" in k:
+                self.logger.info(f"creating index via {k}")
+                getattr(self, f"sync_{k}")()
 
     _autocreating_table = False
-    _autocreating_functions = False
 
     async def execute(
         self,
         qstring: str,
         *args: Any,
-    ) -> list[asyncpg.Record]:
+    ) -> Optional[list[asyncpg.Record]]:
         """Invoke the asyncpg connection's `_execute` given a provided query string and set of arguments"""
-        qstring = qstring.replace("\n", " ").replace(
-            "  ", ""
-        )  # remove whitespace for the logs
         timeout: int = 180
-        if not self.pool.pool and not isinstance(self.database, dict):
-            await self.pool.connect(self.database, self.table, self.database_name)
-        assert self.pool.pool
-        logging.debug('execute("%s" , *%s)', qstring, args)
-        async with self.pool.acquire() as connection:
-            result: tuple[asyncpg.Record, list, bool]
-            try:
+        if not pool.pool and not isinstance(self.database, dict):
+            await pool.connect(self.database, self.table)
+        if pool.pool:
+            async with pool.acquire() as connection:
+                # try:
+                # except asyncpg.TooManyConnectionsError:
+                # await connection.execute(
+                #     """
+                #     SELECT pg_terminate_backend(pg_stat_activity.pid)
+                #     FROM pg_stat_activity
+                #     WHERE pg_stat_activity.datname = 'postgres'
+                #     AND pid <> pg_backend_pid();
+                #     """
+                # )
+                # return self.execute(qstring, *args, timeout=timeout)
                 # _execute takes query, args, limit, timeout
-                result = await connection._execute(
-                    qstring, args, 0, timeout, return_status=True
-                )
-            except asyncpg.TooManyConnectionsError:
-                await connection.execute(
-                    """
-                    SELECT pg_terminate_backend(pg_stat_activity.pid)
-                    FROM pg_stat_activity
-                    WHERE pg_stat_activity.datname = 'postgres'
-                    AND pid <> pg_backend_pid();
-                    """
-                )
-                result = await connection._execute(
-                    qstring, args, 0, timeout, return_status=True
-                )
-            except asyncpg.UndefinedTableError:
-                if self._autocreating_table:
-                    logging.error(
-                        "would try creating the table, but we already tried to do that"
+                try:
+                    result = await connection._execute(
+                        qstring, args, 0, timeout, return_status=True
                     )
-                    raise
-                self._autocreating_table = True
-                logging.info("creating table %s", self.table)
-                await self.create_table()
-                self._autocreating_table = False
-                result = await connection._execute(
-                    qstring, args, 0, timeout, return_status=True
-                )
-            except (asyncpg.UndefinedFunctionError, asyncpg.UndefinedObjectError):
-                if "create_functions" not in self.queries:
-                    logging.error("undefined function and can't autocreate")
-                    raise
-                if self._autocreating_functions:
-                    logging.error(
-                        "would try creating functions, but we already tried to do that"
+                    # list[asyncpg.Record], str, bool
+                except asyncpg.UndefinedTableError:
+                    if self._autocreating_table:
+                        logging.error(
+                            "would try creating the table, but we already tried to do that"
+                        )
+                        raise
+                    self._autocreating_table = True
+                    logging.info("creating table %s", self.table)
+                    await self.create_table()
+                    self._autocreating_table = False
+                    result = await connection._execute(
+                        qstring, args, 0, timeout, return_status=True
                     )
-                    raise
-                self._autocreating_functions = True
-                logging.info("creating functions for %s", self.table)
-                await connection.execute(self.queries.get_query("create_functions"))
-                self._autocreating_table = False
-                result = await connection._execute(
-                    qstring, args, 0, timeout, return_status=True
-                )
-            # list[asyncpg.Record], str, bool
-
-            return result[0]
+                return result[0]
+        return None
 
     def sync_execute(self, qstring: str, *args: Any) -> asyncpg.Record:
         """Synchronous wrapper for `self.execute`"""
@@ -244,9 +204,9 @@ class PGInterface:
         return ret
 
     def sync_close(self) -> Any:
-        logging.info(f"closing connection: {self.pool.pool}")
-        if self.pool.pool:
-            ret = self.loop.run_until_complete(self.pool.pool.close())
+        self.logger.info(f"closing connection: {pool.pool}")
+        if pool.pool:
+            ret = self.loop.run_until_complete(pool.pool.close())
             return ret
         return None
 
@@ -255,7 +215,7 @@ class PGInterface:
         if len(thing) > self.MAX_RESP_LOG_LEN:
             return (
                 f"{thing[:self.MAX_RESP_LOG_LEN]}..."
-                f"[{len(thing)-self.MAX_RESP_LOG_LEN} omitted]"
+                "[{len(thing)-self.MAX_RESP_LOG_LEN} omitted]"
             )
         return thing
 
@@ -280,7 +240,7 @@ class PGInterface:
             statement = self.queries.get_query(qstring)
         except KeyError as e:
             raise ValueError(f"No statement of name {qstring} or {key} found!") from e
-        if not self.pool.pool and isinstance(self.database, dict):
+        if not pool.pool and isinstance(self.database, dict):
             canned_response = self.database.get(qstring, [[None]]).pop(0)
             if qstring in self.database and not self.database.get(qstring, []):
                 self.database.pop(qstring)
@@ -292,7 +252,7 @@ class PGInterface:
                 else:
                     resp = canned_response
                 short_strresp = self.truncate(f"{resp}")
-                logging.info(
+                self.logger.info(
                     f"returning `{short_strresp}` for expression: "
                     f"`{qstring}` eval'd with `{args}` & `{kwargs}`"
                 )
@@ -305,9 +265,7 @@ class PGInterface:
                 """Closure over 'statement' in local state for application to arguments.
                 Allows deferred execution of f-strs, allowing PGExpresssions to operate on `args`."""
                 start_time = time.time()
-                rebuilt_statement = eval(
-                    f'f"""{statement}"""'
-                )  # pylint: disable=eval-used
+                rebuilt_statement = eval(f'f"{statement}"')  # pylint: disable=eval-used
                 if (
                     rebuilt_statement != statement
                     and "args" in statement
@@ -317,7 +275,9 @@ class PGInterface:
                 resp = executer(rebuilt_statement, *args)
                 short_strresp = self.truncate(f"{resp}")
                 short_args = self.truncate(str(args))
-                logging.debug(f"{rebuilt_statement} {short_args} -> {short_strresp}")
+                self.logger.debug(
+                    f"{rebuilt_statement} {short_args} -> {short_strresp}"
+                )
                 elapsed = f"{time.time() - start_time:.4f}s"  # round to miliseconds
                 logging.info(
                     "query %s took %s", self.truncate(rebuilt_statement), elapsed
--- utils.py
+++ utils.py
@@ -1,13 +1,38 @@
-#!/usr/bin/python3.10
-# Copyright (c) 2022 sylv
+#!/usr/bin/python3.9
+# Copyright (c) 2021 MobileCoin Inc.
 # Copyright (c) 2021 The Forest Team
-import asyncio
 import functools
 import logging
+import shutil
 import os
-import time
-from functools import _HashedSeq  # type: ignore # it's not in __all__ so mypy is sad
-from typing import Awaitable, Callable, Dict, Optional, ParamSpec, TypeVar, cast
+from pathlib import Path
+from typing import Optional, cast, Dict
+import phonenumbers as pn
+from phonenumbers import NumberParseException
+
+
+def FuckAiohttp(record: logging.LogRecord) -> bool:
+    str_msg = str(getattr(record, "msg", ""))
+    if "was destroyed but it is pending" in str_msg:
+        return False
+    if str_msg.startswith("task:") and str_msg.endswith(">"):
+        return False
+    return True
+
+
+logger_class = logging.getLoggerClass()
+
+logger = logging.getLogger()
+logger.setLevel("DEBUG")
+fmt = logging.Formatter("{levelname} {module}:{lineno}: {message}", style="{")
+console_handler = logging.StreamHandler()
+console_handler.setLevel(
+    ((os.getenv("LOGLEVEL") or os.getenv("LOG_LEVEL")) or "DEBUG").upper()
+)
+console_handler.setFormatter(fmt)
+console_handler.addFilter(FuckAiohttp)
+logger.addHandler(console_handler)
+logging.getLogger("asyncio").setLevel("INFO")
 
 #### Configure Parameters
 
@@ -29,7 +54,7 @@ def parse_secrets(secrets: str) -> dict[str, str]:
 @functools.cache  # don't load the same env more than once
 def load_secrets(env: Optional[str] = None, overwrite: bool = False) -> None:
     if not env:
-        env = os.environ.get("ENV", "dev").lower()
+        env = os.environ.get("ENV", "dev")
     try:
         logging.info("loading secrets from %s_secrets", env)
         secrets = parse_secrets(open(f"{env}_secrets").read())
@@ -47,19 +72,17 @@ secret_cache: Dict[str, str] = {}
 
 # potentially split this into get_flag and get_secret; move all of the flags into fly.toml;
 # maybe keep all the tomls and dockerfiles in a separate dir with a deploy script passing --config and --dockerfile explicitly
-def get_secret(key: str, env: Optional[str] = None, fail_if_none: bool = False) -> str:
+def get_secret(key: str, env: Optional[str] = None) -> str:
+    if key in secret_cache:
+        return secret_cache[key]
     try:
         secret = os.environ[key]
     except KeyError:
         load_secrets(env)
-        secret = os.environ.get(key, "")
-        if not secret:
-            if fail_if_none:
-                raise ValueError(f"Unable to find secret {key}, got {secret}")
-            secret = ""
+        secret = os.environ.get(key) or ""  # fixme
+        secret_cache[key] = secret
     if secret.lower() in ("0", "false", "no"):
         return ""
-
     return secret
 
 
@@ -68,80 +91,44 @@ def get_secret(key: str, env: Optional[str] = None, fail_if_none: bool = False) 
 APP_NAME = os.getenv("FLY_APP_NAME")
 URL = os.getenv("URL_OVERRIDE", f"https://{APP_NAME}.fly.dev")
 LOCAL = os.getenv("FLY_APP_NAME") is None
-ENV = os.getenv("ENV", "DEV")
+DOWNLOAD = get_secret("DOWNLOAD")
+UPLOAD = get_secret("UPLOAD") or DOWNLOAD
+ROOT_DIR = get_secret("ROOT_DIR") or (
+    "/app" if not LOCAL else "/tmp/local-signal" if DOWNLOAD else "."
+)
+MEMFS = get_secret("AUTOSAVE")
+SIGNAL = (get_secret("SIGNAL") or "auxin").removesuffix("-cli") + "-cli"
+AUXIN = SIGNAL.lower() == "auxin-cli"
+
+maybe_path = get_secret("SIGNAL_PATH")
+if maybe_path and Path(maybe_path).exists():
+    SIGNAL_PATH = str(Path(maybe_path).absolute())
+elif Path(SIGNAL).exists():
+    SIGNAL_PATH = str(Path(SIGNAL).absolute())
+elif (Path(ROOT_DIR) / SIGNAL).exists():
+    SIGNAL_PATH = str((Path(ROOT_DIR) / SIGNAL).absolute())
+elif which := shutil.which(SIGNAL):
+    SIGNAL_PATH = which
+elif os.getenv("ENV") == "test":
+    SIGNAL_PATH = SIGNAL  # doesn't matter, just use something
+else:
+    raise FileNotFoundError(
+        f"Couldn't find a {SIGNAL} executable in the working directory, {ROOT_DIR}, or as an executable in PATH "
+        f"Install {SIGNAL} or try symlinking {SIGNAL} to the working directory"
+    )
+
+#### Configure logging to file
+
+if get_secret("LOGFILES") or not LOCAL:
+    handler = logging.FileHandler("debug.log")
+    handler.setLevel("DEBUG")
+    handler.setFormatter(fmt)
+    handler.addFilter(FuckAiohttp)
+    logger.addHandler(handler)
 
 
-def log_task_result(task: asyncio.Task) -> None:
-    """
-    Done callback which logs task done result
-    args:
-        task (asyncio.task): task to be handled
-    """
-    name = task.get_name() + "-" + getattr(task.get_coro(), "__name__", "")
+def signal_format(raw_number: str) -> Optional[str]:
     try:
-        result = task.result()
-        logging.info("final result of %s was %s", name, result)
-    except asyncio.CancelledError:
-        logging.info("task %s was cancelled", name)
-    except Exception:  # pylint: disable=broad-except
-        logging.exception("%s errored", name)
-
-def wrap_task(task: asyncio.Task) -> asyncio.Task:
-    task.add_done_callback(log_task_result)
-    return task
-
-
-# def cached(ttl: int) -> Callable[[Callable[P, T]], Callable[P, T]]:
-#     "decorator to cache function with a ttl"
-#
-#     def decorator(fn: Callable[P, T]) -> Callable[P, T]:
-#         @functools.lru_cache
-#         def cached_fn(*args: P.args, ttl_hash: int, **kwargs: P.kwargs) -> T:
-#             del ttl_hash
-#             return fn(*args, **kwargs)
-#
-#         @functools.wraps(fn)
-#         def caching_wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-#             # ttl_hash is the same value within `timeout` time period
-#
-#             ttl_hash = round(time.time() / ttl)
-#             return cached_fn(*args, ttl_hash=ttl_hash, **kwargs)
-#
-#         return caching_wrapper
-
-#     return decorator
-
-T = TypeVar("T")
-P = ParamSpec("P")
-
-
-def swr_cache(
-    ttl: int = 60, swr_ttl: int = 3600
-) -> Callable[[Callable[P, Awaitable[T]]], Callable[P, Awaitable[T]]]:
-    "ttl: how long a result stays fresh. swr_ttl: how long a result can be returned while revalidating"
-
-    def decorator(fn: Callable[P, Awaitable[T]]) -> Callable[P, Awaitable[T]]:
-        cache: dict[_HashedSeq, tuple[int, int, T]] = {}
-
-        async def revalidate(key: _HashedSeq, *args: P.args, **kwargs: P.kwargs) -> T:
-            value = await fn(*args, **kwargs)
-            cache[key] = (round(time.time() / ttl), round(time.time() / swr_ttl), value)
-            return value
-
-        @functools.wraps(fn)
-        async def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
-            # build a quickly hashable key from the function arguments as per lru_cache
-            key = functools._make_key(args, kwargs, typed=False)
-            ttl_key, swr_ttl_key, value = cache.get(key, (None, None, None))
-            if value and swr_ttl_key == round(time.time() / swr_ttl):
-                logging.info("swr ttl key matches")
-                if ttl_key != round(time.time() / ttl):
-                    logging.info("returning stale while revalidating for %s", fn)
-                    asyncio.create_task(revalidate(key, *args, **kwargs))
-                logging.info("ttl key matches, not revalidating")
-                return value
-            return await revalidate(key, *args, **kwargs)
-
-        return wrapped
-
-    return decorator
+        return pn.format_number(pn.parse(raw_number, "US"), pn.PhoneNumberFormat.E164)
+    except NumberParseException:
+        return None
